2a7c5d65f04133e041f86f4c86feadb5612e8e1f
Amrita Vishwakarma <amrita9463360@gmail.com>
2025-03-03 15:27:39 +0530

    initial commit

---
+++ src/components/FirstCode.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="first">
+<h1>Amrita Vishwkarma</h1>
+<p>This is my first vue Project</p>
+
+</div>
+</template>
---
+++ vetur.config.js
@@ -0,0 +1,13 @@
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    projects: [
+      {
+        root: './',
+        package: './package.json',
+        tsconfig: './tsconfig.json',
+        snippetFolder: './.vscode/vetur/snippets',
+        globalComponents: ['./src/components/**/*.vue']
+      }
+    ]
+  };
+  

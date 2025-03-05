3db2e878b5542c6c95187dcae43800db63701f56
Amrita Vishwakarma <amrita9463360@gmail.com>
2025-03-05 12:02:37 +0530

    This is fith test includes lifecycle and date with input field

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <TestFirst/>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <FirstCode/>
+    <test-fifth/>
   </div>
 </template>
 
 <script>
-
-import FirstCode from './components/FirstCode.vue'
-import TestFirst from './components/TestFirst.vue'
-
+import TestFifth from './components/Test-fifth.vue'
 export default {
   name: 'App',
   components: {
-    TestFirst,
-    FirstCode,
-
+    TestFifth
   }
 }
 </script>
---
+++ src/components/Test-fifth.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="testing">
+        {{name}}Testing
+        <br/>
+        <br/>
+        {{date}}
+        <br>
+        <input type="text" v-model="name"/>
+        <button v-on:click="clear">Clear</button>
+        <button v-on:click="makeCapital">Caps</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"Test-fifth",
+    data:()=>{
+        return{
+            name:"Amrita",
+            date:false
+        }
+},
+created:function(){
+    console.log("created");
+    this.date=new Date();
+},
+mounted:function(){
+    console.log("mounted");
+    this.date=new Date();
+},
+updated:function(){
+    console.log("updated");
+},
+destroyed:function(){
+    console.log("destroyed")
+    this.message=new Date();
+},
+methods:{
+    clear:function(){
+        this.name=""
+    },
+    makeCapital:function(){
+        this.name=this.name.toUpperCase()
+    }
+}
+
+
+}
+</script>
+<style>
+</style>
